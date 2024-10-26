cc35ba4c721acbbd1de5aea7d2b04638d512fd32
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2024-10-27 02:29:49 +0800

    Update AdvancedCrafterBlockEntity.java

--- src/main/java/thelm/packagedexcrafting/block/entity/AdvancedCrafterBlockEntity.java
+++ src/main/java/thelm/packagedexcrafting/block/entity/AdvancedCrafterBlockEntity.java
@@ -13,7 +13,6 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.CraftingInput;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.capabilities.Capabilities;
